31992ed931d732d86081848f327fc8f7f7bceea0
James E. Jones <james.jones@puppetlabs.com>
2016-02-23 20:35:22 -0600

    update etcd default config

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,13 +4,13 @@ class etcd::config {
   case $::osfamily {
     'RedHat' : {
       case $::operatingsystemmajrelease {
-        6       : {
+        '6'       : {
           file { '/etc/sysconfig/etcd.conf':
             ensure  => 'file',
             content => template("${module_name}/etc/etcd/etcd.conf.erb"),
           }
         }
-        7       : {
+        '7'       : {
           file { '/etc/etcd/': ensure => 'directory', } ->
           file { '/etc/etcd/etcd.conf':
             ensure  => 'file',
